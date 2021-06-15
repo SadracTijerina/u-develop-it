c78432490cf169089ba86e3bd6c74580a7c55428
Sadrac Tijerina <reavity3@gmail.com>
2021-06-15 07:56:07 -0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: "root",
     // Your MySQL password
-    password: "sadrac780490",
+    password: "afdafafc",
     database: "election",
   },
   console.log("Connected to the election database.")
